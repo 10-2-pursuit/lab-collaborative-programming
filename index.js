a2ee64324f17a4e7e087acf3d3d42c65bc07f778
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-05-24 13:31:45 -0400

    completed

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+function sortByStringLength(words) {
+  if (!Array.isArray(words)) {
+    return words;
+  }
+
+  for (let word of words) {
+    if (typeof word !== "string") {
+      return words;
+    }
+  }
+
+  return words.sort((a, b) => a.length - b.length);
+}
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +26,37 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(word) {
+  if (word === '') {
+    return [];
+  }
+
+  const scrollingPositions = [];
+
+  for (let i = 1; i <= word.length; i++) {
+    const scrolledWord = word.slice(i) + word.slice(0, i);
+    scrollingPositions.push(scrolledWord);
+  }
+
+  return scrollingPositions;
+}
+
+
+
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(numbers) {
+  if (numbers.some((num) => typeof num !== 'number')) {
+    return numbers;
+  }
+
+  return Math.max(...numbers) - Math.min(...numbers);
+}
+
 
 /**
  * Returns the difference between the largest and smallest number in the array
@@ -31,7 +66,30 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message, dictionary) {
+  const words = message.toUpperCase().split(' ');
+  const translatedMessage = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const translatedWord = [];
+
+    for (let j = 0; j < word.length; j++) {
+      const char = word[j];
+
+      if (dictionary[char]) {
+        translatedWord.push(dictionary[char]);
+      }
+    }
+
+    translatedMessage.push(translatedWord.join(' '));
+  }
+
+  return translatedMessage.join(' ');
+}
+
+
+
 
 module.exports = {
   sortByStringLength,
