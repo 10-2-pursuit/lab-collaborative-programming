bf1980950e2f7cc292500dad3ebe11c2b44b1526
Tonesharose31 <tonesharose@pursuit.org>
2023-05-24 17:47:55 -0400

    Completed

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+ function sortByStringLength(stringsOfArr) {
+  return stringsOfArr.sort((a,b)=>(a.length-b.length))
+// 
+
+  // let newArr= [];
+  // for(let arr of string){
+  //   if(!string){
+  //     return newArr 
+  //   }
+  // }
+}
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +24,37 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(word){
+let scrollWord = [];
+  if(word.length < 1){
+    return scrollWord
+  }
+for(let i = 1; i < word.length; i++) {
+  const tempWord = word.slice(i)+ word.slice(0,i)
+  scrollWord.push(tempWord)
+ 
+}
+scrollWord.push(word)
+return scrollWord
+}
+
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(numbers) {
+if(numbers.length === 0){
+  return [];
+}
+if(!numbers.every(a => typeof(a) == "number")){
+  return numbers; 
+}
+numbers = numbers.sort ((a,b) => a-b);
+return numbers[numbers.length -1]- numbers[0]; 
+
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
@@ -31,7 +64,17 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message, dictionary) {
+  let translatedString = []; 
+
+  message = message.replaceAll(" ", "")
+  for (let words of message) {
+translatedString.push(dictionary[words.toUpperCase()]);
+  
+}
+return translatedString.join(" ");
+
+}
 
 module.exports = {
   sortByStringLength,
