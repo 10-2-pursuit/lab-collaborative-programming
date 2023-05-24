18c29667a202cf2e0bea322d46d145dcc0d2f8fb
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-05-24 15:04:55 -0400

    finished collab

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+function sortByStringLength(stringArray) {
+  
+  return stringArray.sort((a, b)=> a.length - b.length);
+}
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +17,38 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(string) {
+  let scrollStr = [];
+  let tempString;
+
+  for (let i = 0; i < string.length; i++) {
+    tempString = '';
+
+    for (let j = 1; j <= string.length; j++) {
+      tempString += string[(i + j) % string.length];//% keep from adding extra chars
+    }
+    scrollStr.push(tempString);
+  }
+  return scrollStr;
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(numbers) {
+
+  const result = numbers.every(element => {
+    return typeof element === 'number';
+  });
+
+  if (!result) {
+    return numbers;
+  } else {
+    return Math.max(...numbers) - Math.min(...numbers);
+  }
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
@@ -31,7 +58,24 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message, dictionary) {
+  message = message.replace(/ /g, "");
+  let splitMessage = message.toUpperCase().split('');
+  let codeString = '';
+
+  for(let i = 0; i <= splitMessage.length; i++) {
+    for (let letter in dictionary) {
+      if (letter == splitMessage[i]) {
+        codeString += dictionary[letter] + ' ';
+      }
+    }
+  }
+
+  codeString = codeString.trim();
+
+  console.log(codeString)
+  return codeString;
+}
 
 module.exports = {
   sortByStringLength,
