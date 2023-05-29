3913352c97cd77f135ea318fba28bcd1893420dc
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-05-29 11:27:28 -0400

    Completes Collab Programming Lab for Gen and Ant.

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+function sortByStringLength(sentence) {
+ sentence.sort((a, b) => a.length - b.length)
+ return sentence
+}
+
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +18,32 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(word) {
+    textArr = []
+    tempText = ""
+    for (i = 1; i < word.length + 1; i++) {
+    tempText = word.slice(i) + word.slice(0, i)
+    textArr.push(tempText)
+    }
+    return textArr
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(numbers) {
+  if(numbers.length === 0) {
+    return []
+  }
+  for(let num of numbers){
+    if(typeof num !== 'number'){
+    return numbers
+  }  
+}
+return Math.max(...numbers) - Math.min(...numbers)
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
@@ -31,7 +53,14 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message, dictionary) {
+  let morseCodeMessage = []
+  message = message.replaceAll(" ", "");
+  for(let words of message) {
+    morseCodeMessage.push(dictionary[words.toUpperCase()])
+  }
+  return morseCodeMessage.join(" ")
+}
 
 module.exports = {
   sortByStringLength,
